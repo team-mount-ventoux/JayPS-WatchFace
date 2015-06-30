4e5df18c51ff0b9cdaf93d495ed9679c79272c92
JayPS <dev@jayps.fr>
2015-06-30 22:02:53 +0200

    v2.1.0-alpha2

--- pebblebike/src/config.h
+++ pebblebike/src/config.h
@@ -4,8 +4,8 @@
 #define PRODUCTION true
 
 
-#define VERSION_PEBBLE 250
-#define VERSION_TEXT "Version 2.1.0-alpha1"
+#define VERSION_PEBBLE 251
+#define VERSION_TEXT "Version 2.1.0-alpha2"
 #define APP_COMPANY "N Jackson & JayPS"
 
 #if PRODUCTION
